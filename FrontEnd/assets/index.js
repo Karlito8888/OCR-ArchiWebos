02db91139b521a9386b3cbcd4907e7195e5c33a8
Karlito8888 <charles.bourgault@hotmail.fr>
2024-08-12 16:49:18 +0200

    Fin de la mise en page de la page d'accueil en mode connexion

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,105 +1,134 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const gallery = document.querySelector('.gallery');
-    const categoriesContainer = document.querySelector('.categories');
-    let works = [];
-    let categories = [];
+document.addEventListener("DOMContentLoaded", () => {
+  const gallery = document.querySelector(".gallery");
+  const categoriesContainer = document.querySelector(".categories");
+  let works = [];
+  let categories = [];
 
-     // Vérifie si l'utilisateur est connecté
-    const loggedIn = localStorage.getItem('loggedIn') === 'true';
-    const token = localStorage.getItem('token');
+  // Vérifie si l'utilisateur est connecté
+  const loggedIn = localStorage.getItem("loggedIn") === "true";
+  const token = localStorage.getItem("token");
 
-    const loginButton = document.querySelector('nav ul li a[href="login.html"]');
+  const loginButton = document.querySelector('nav ul li a[href="login.html"]');
+  const headerContainer = document.querySelector(".header-container");
+  const editModeHeader = document.querySelector("header .edit-mode");
+  const modifyEditMode = document.querySelector(".modify-edit-mode");
 
-    if (loggedIn) {
-        loginButton.textContent = 'logout';
-        loginButton.href = '#';
-        document.querySelector('header .edit-mode').classList.add('active');
+  function isUserLoggedIn() {
+    return localStorage.getItem("loggedIn") === "true";
+  }
+
+  function applyUserLayout() {
+    if (isUserLoggedIn()) {
+      editModeHeader.style.height = "59px";
+      headerContainer.style.margin = "38px auto 92px";
+      loginButton.textContent = "logout";
+      loginButton.href = "login.html";
+      modifyEditMode.style.display = "block";
+      categoriesContainer.style.display = "none";
+      gallery.style.marginTop = "92px";
+    } else {
+      editModeHeader.style.height = "0";
+      headerContainer.style.margin = "50px auto 139px";
+      loginButton.textContent = "login";
+      loginButton.href = "login.html";
+      modifyEditMode.style.display = "none";
+      categoriesContainer.style.display = "flex";
+      gallery.style.marginTop = "0";
     }
+  }
 
-    // Fonction pour créer et ajouter les figures dans la galerie
-    function createGalleryItem(item) {
-        const figure = document.createElement('figure');
-        const img = document.createElement('img');
-        img.src = item.imageUrl;
-        img.alt = item.title;
-        const figcaption = document.createElement('figcaption');
-        figcaption.textContent = item.title;
+  if (loggedIn) {
+    applyUserLayout();
+  }
 
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        gallery.appendChild(figure);
-    }
+  // Fonction pour créer et ajouter les figures dans la galerie
+  function createGalleryItem(item) {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    img.src = item.imageUrl;
+    img.alt = item.title;
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = item.title;
 
-     // Fonction pour créer les boutons de catégories
-    function createCategoryButtons(categories) {
-        const allButton = document.createElement('button');
-        allButton.textContent = 'Tous';
-        allButton.classList.add('active');
-        allButton.addEventListener('click', () => {
-            setActiveButton(allButton);
-            displayWorks(works);
-        });
-        categoriesContainer.appendChild(allButton);
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+  }
 
-
-        categories.forEach(category => {
-            const button = document.createElement('button');
-            button.textContent = category.name;
-             button.addEventListener('click', () => {
-                setActiveButton(button);
-                filterWorksByCategory(category.id);
-            });
-            categoriesContainer.appendChild(button);
-        });
-    }
-
-    // Fonction pour définir le bouton actif
-    function setActiveButton(button) {
-        const buttons = categoriesContainer.querySelectorAll('button');
-        buttons.forEach(btn => btn.classList.remove('active'));
-        button.classList.add('active');
-    }
-
-    // Fonction pour afficher les travaux
-    function displayWorks(worksToDisplay) {
-        gallery.innerHTML = ''; // Clear existing works
-        worksToDisplay.forEach(item => createGalleryItem(item));
-    }
-
-    // Fonction pour filtrer les travaux par catégorie
-    function filterWorksByCategory(categoryId) {
-        const filteredWorks = works.filter(work => work.categoryId === categoryId);
-        displayWorks(filteredWorks);
-    }
-
-    // Fonction pour récupérer les données de l'API
-   function fetchData() {
-        fetch('http://localhost:5678/api/works/')
-            .then(response => response.json())
-            .then(data => {
-                works = data; // Stocker les travaux récupérés
-                displayWorks(works); // Afficher tous les travaux initialement
-
-                // Extraire les catégories uniques en utilisant Set et les onvertir le Set en tableau.
-
-                const categoryIds = [...new Set(works.map(work => work.categoryId))];
-                categories = categoryIds.map(id => works.find(work => work.category.id === id).category);
-                createCategoryButtons(categories);
-            })
-            .catch(error => console.error('Erreur lors de la récupération des données:', error));
-    }
-
-    // Appeler la fonction pour récupérer les données
-    fetchData();
-
-     loginButton.addEventListener('click', (event) => {
-        if (loggedIn) {
-            event.preventDefault();
-            localStorage.removeItem('loggedIn');
-            localStorage.removeItem('token');
-            loginButton.textContent = 'login';
-            loginButton.href = 'login.html';
-            document.querySelector('header .edit-mode').classList.remove('active');
-        }
+  // Fonction pour créer les boutons de catégories
+  function createCategoryButtons(categories) {
+    const allButton = document.createElement("button");
+    allButton.textContent = "Tous";
+    allButton.classList.add("active");
+    allButton.addEventListener("click", () => {
+      setActiveButton(allButton);
+      displayWorks(works);
     });
+    categoriesContainer.appendChild(allButton);
+
+    categories.forEach((category) => {
+      const button = document.createElement("button");
+      button.textContent = category.name;
+      button.addEventListener("click", () => {
+        setActiveButton(button);
+        filterWorksByCategory(category.id);
+      });
+      categoriesContainer.appendChild(button);
+    });
+  }
+
+  // Fonction pour définir le bouton actif
+  function setActiveButton(button) {
+    const buttons = categoriesContainer.querySelectorAll("button");
+    buttons.forEach((btn) => btn.classList.remove("active"));
+    button.classList.add("active");
+  }
+
+  // Fonction pour afficher les travaux
+  function displayWorks(worksToDisplay) {
+    gallery.innerHTML = ""; // Clear existing works
+    worksToDisplay.forEach((item) => createGalleryItem(item));
+  }
+
+  // Fonction pour filtrer les travaux par catégorie
+  function filterWorksByCategory(categoryId) {
+    const filteredWorks = works.filter(
+      (work) => work.categoryId === categoryId
+    );
+    displayWorks(filteredWorks);
+  }
+
+  // Fonction pour récupérer les données de l'API
+  function fetchData() {
+    fetch("http://localhost:5678/api/works/")
+      .then((response) => response.json())
+      .then((data) => {
+        works = data; // Stocker les travaux récupérés
+        displayWorks(works); // Afficher tous les travaux initialement
+
+        // Extraire les catégories uniques en utilisant Set et les onvertir le Set en tableau.
+
+        const categoryIds = [...new Set(works.map((work) => work.categoryId))];
+        categories = categoryIds.map(
+          (id) => works.find((work) => work.category.id === id).category
+        );
+        createCategoryButtons(categories);
+      })
+      .catch((error) =>
+        console.error("Erreur lors de la récupération des données:", error)
+      );
+  }
+
+  // Appeler la fonction pour récupérer les données
+  fetchData();
+
+  loginButton.addEventListener("click", (event) => {
+    if (loggedIn) {
+      event.preventDefault();
+      localStorage.removeItem("loggedIn");
+      localStorage.removeItem("token");
+      applyUserLayout();
+      window.location.href = "login.html";
+    }
+  });
 });
--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,65 +1,61 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('loginForm');
-    const loginButton = document.querySelector('nav ul li a[href="login.html"]');
-    const editModeHeader = document.querySelector('header .edit-mode');
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("loginForm");
+  const loginButton = document.querySelector('nav ul li a[href="login.html"]');
 
-     // Vérifie si l'utilisateur est déjà connecté
-    if (localStorage.getItem('loggedIn') === 'true') {
-        loginButton.textContent = 'logout';
-        loginButton.href = '#';
-        editModeHeader.classList.add('active');
+  // Vérifie si l'utilisateur est déjà connecté
+  if (localStorage.getItem("loggedIn") === "true") {
+    loginButton.textContent = "logout";
+    loginButton.href = "#";
+  }
+
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault(); // Empêche le comportement par défaut du formulaire
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      const response = await fetch("http://127.0.0.1:5678/api/users/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Erreur de connexion");
+      }
+
+      const data = await response.json();
+      console.log("Connexion réussie:", data);
+
+      // Sauvegarder le token dans localStorage
+      localStorage.setItem("token", data.token);
+
+      // Sauvegarder l'état de connexion dans localStorage
+      localStorage.setItem("loggedIn", "true");
+
+      // Modifier le bouton login en logout et activer le mode édition
+      loginButton.textContent = "logout";
+      loginButton.href = "#";
+
+      // Rediriger vers index.html
+      window.location.href = "index.html";
+    } catch (error) {
+      console.error("Erreur:", error);
+      // Affiche un message d'erreur à l'utilisateur
     }
+  });
 
-    form.addEventListener('submit', async (event) => {
-        event.preventDefault(); // Empêche le comportement par défaut du formulaire
-
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-
-        try {
-            const response = await fetch('http://127.0.0.1:5678/api/users/login/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ email, password })
-            });
-
-            if (!response.ok) {
-                throw new Error('Erreur de connexion');
-            }
-
-            const data = await response.json();
-            console.log('Connexion réussie:', data);
-
-            // Sauvegarder le token dans localStorage
-            localStorage.setItem('token', data.token);
-
-            // Sauvegarder l'état de connexion dans localStorage
-            localStorage.setItem('loggedIn', 'true');
-
-            // Modifier le bouton login en logout et activer le mode édition
-            loginButton.textContent = 'logout';
-            loginButton.href = '#';
-            editModeHeader.classList.add('active');
-
-            // Rediriger vers index.html
-            window.location.href = 'index.html';
-        } catch (error) {
-            console.error('Erreur:', error);
-            // Affiche un message d'erreur à l'utilisateur
-        }
-    });
-
-     // Gestion du clic sur le bouton logout
-    loginButton.addEventListener('click', (event) => {
-        if (localStorage.getItem('loggedIn') === 'true') {
-            event.preventDefault();
-            localStorage.removeItem('loggedIn');
-            localStorage.removeItem('token');
-            loginButton.textContent = 'login';
-            loginButton.href = 'login.html';
-            editModeHeader.classList.remove('active');
-        }
-    });                                        
+  // Gestion du clic sur le bouton logout
+  loginButton.addEventListener("click", (event) => {
+    if (localStorage.getItem("loggedIn") === "true") {
+      event.preventDefault();
+      localStorage.removeItem("loggedIn");
+      localStorage.removeItem("token");
+      loginButton.textContent = "login";
+      loginButton.href = "login.html";
+    }
+  });
 });
